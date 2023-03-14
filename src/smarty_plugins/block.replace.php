f91938c62a73d670c9eb313aadfda8b1126e0bc7
yusam <yusam@yandex.ru>
2023-03-14 09:47:39 +0400

    #changed

---
+++ src/smarty_plugins/block.replace.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Smarty plugin to format text blocks
+ *
+ * @package    Smarty
+ * @subpackage PluginsBlock
+ */
+/**
+ * @param array $params   parameters
+ * @param string $content  contents of the block
+ * @param Smarty_Internal_Template $template template object
+ * @param boolean &$repeat  repeat flag
+ *
+ * @return string content
+ */
+function smarty_block_replace($params, $content, Smarty_Internal_Template $template, &$repeat)
+{
+    return str_replace($params['search']??'', $params['replace']??'', $content);
+}
